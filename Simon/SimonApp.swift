113f945b36207831e72eec9f9f6ab2eeb9814b49
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-22 17:17:51 -0500

    Initial Commit

---
+++ Simon/SimonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimonApp.swift
+//  Simon
+//
+//  Created by Pamela VanLeirsburg on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SimonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
