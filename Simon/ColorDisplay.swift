428a685046ab4d4d9a9e114575706f928b0583f2
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-31 16:00:59 -0500

    Created the color grid and flash array

---
+++ Simon/ColorDisplay.swift
@@ -0,0 +1,18 @@
+//
+//  ColorDisplay.swift
+//  Simon
+//
+//  Created by Pamela VanLeirsburg on 7/31/26.
+//
+
+import SwiftUI
+
+struct ColorDisplay: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorDisplay()
+}
