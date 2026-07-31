26d2f6a859d4850c0ce3eee6a1e0db7cfea31096
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-31 17:34:53 -0500

    Began StartView

---
+++ Simon/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  Simon
+//
+//  Created by Pamela VanLeirsburg on 7/31/26.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartView()
+}
