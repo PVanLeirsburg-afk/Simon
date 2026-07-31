4cd114c5dadd4c2d3556c6b34a025c7e4690628e
Pam VanLeirsburg <pvanleirsburg@barrington220.org>
2026-07-31 15:31:39 -0500

    Created black background and title text

--- Simon/ContentView.swift
+++ Simon/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+
+            Text("simon")
+                .font(.system(size: 72))
+                .foregroundColor(.white)
         }
-        .padding()
+            .preferredColorScheme(.dark)
     }
 }
 
